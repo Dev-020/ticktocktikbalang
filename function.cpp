b1659463ab4f8b61ecbcc4310a0fc13d95dcccc3
Dev-020 <82759695+Dev-020@users.noreply.github.com>
2024-05-22 14:08:22 +0800

    Default Files
    Added base files to be configured later

---
+++ function.cpp
@@ -0,0 +1,9 @@
+#include "header.h"
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello Tikbalang!"<<endl;
+    return 0;
+}
---
+++ header.h
@@ -0,0 +1,8 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+// To be added
+#include <iostream>
+#include <stdio.h>
+
+#endif
