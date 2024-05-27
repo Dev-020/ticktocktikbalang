1a008b5ba704e81494ab21f970cbf41a8d5e2d8d
Dev-020 <82759695+Dev-020@users.noreply.github.com>
2024-05-28 01:59:07 +0800

    Setup SFML in project
    Setup the foundation for the SFML library in the project.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,11 +1,25 @@
 #include "header.h"
-
-using namespace std;
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    cout<<"Hello Tikbalang!"<<endl;
-    cout<<"Testing New Change"<<endl;
-    cout<<"New Change"<<endl;
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
+    sf::CircleShape shape(200.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
